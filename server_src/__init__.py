bc9717fe0dcb4ec8db29a0d936619fde00e2445b
Shariq Torres <shariq.torres@gmail.com>
2025-12-30 00:23:48 -0800

    fixed import issue

--- server_src/__init__.py
+++ server_src/__init__.py
@@ -93,13 +93,13 @@ def process_admin():
 @app.route("/supmod", methods=['GET', 'POST'])
 def login_moderator():
   if request.method == 'GET':
-    if utils.check_key("firstpw") == 1:
+    if dbsetup.check_key("firstpw") == 1:
       return redirect("/admin_setup")
     return render_template("mod_login.html")
 
   if request.method == 'POST':
-    # if request.form['skip'] == "True" and os.environ['MODE'] == 'DEBUG':
-    #   return render_template("mod_response.html", message="12345")
+    if request.form['skip'] == "True" and os.environ['MODE'] == 'DEBUG':
+      return render_template("mod_response.html", message="12345")
     session.pop('mod_number', None)
     pubkey = garden.create_key_from_text(request.form['pubkey'].strip())
     superuser_key = datastore.get_admin_publickey()
@@ -125,10 +125,10 @@ def login_moderator():
 def code_response():
   approved = False
 
-  # if request.form['password'] == "12345" and os.environ['MODE'] == 'DEBUG':
-  #   approved = True
-  #   session['is_mod'] = True
-  #   return redirect("/dashboard")
+  if request.form['password'] == "12345" and os.environ['MODE'] == 'DEBUG':
+    approved = True
+    session['is_mod'] = True
+    return redirect("/dashboard")
   
   password = request.form['password'] + "/" + str(session['mod_number'])
   codes = dbsetup.get_active_login_codes()
